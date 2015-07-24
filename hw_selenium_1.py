30cf4a81a0c6cf7858ea54f0237aaf08e4b98b84
Serhii Aksiutin <saksiutin@scml-serhii.chegg.com>
2015-07-23 17:17:52 -0700

    clrea code - no Implicit Waits

--- hw_selenium_1.py
+++ hw_selenium_1.py
@@ -8,8 +8,8 @@ import time
 import unicodedata
 
 # Running Chrome browser
-browser = webdriver.Chrome()  # = webdriver.Firefox()
-
+# browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 # Assigning URL
 URL = 'http://www.dice.com/'
 
@@ -27,35 +27,40 @@ search_field.send_keys(keyword)
 find_job_button = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/form/fieldset/div[4]/div/div[1]/button')
 find_job_button.click()
 
-# Making a pause for 'Sign Up For Job Alerts' pop up window
+# Making a pause for 'Sign Up For Job Alerts' pop up window... \Implicit Waits needs to be implemented
 time.sleep(8)
 
 # Verifying if pop up appears
-check_pop_up = browser.find_element_by_id('myModalLabel')
+try:
+    check_pop_up = browser.find_element_by_id('myModalLabel')
+    pop_up_title = check_pop_up.text
+    print("The '%s' pop up appears" % (pop_up_title))
 
-pop_up_title = check_pop_up.text
-print("The '%s' pop up appears" % (pop_up_title))
-
-# Closing the pop up window once it appears
-if pop_up_title == 'Sign Up For Job Alerts':
-    close_button = browser.find_element_by_xpath('//*[@id="myModal"]/div/div/div[1]/button').click()
-    print('The pop up was closed')
-else:
-    raise Exception("Timeout while waiting the pop up. Please run the test again.")
+    # Closing the pop up window once it appears
+    if pop_up_title == 'Sign Up For Job Alerts':
+        close_button = browser.find_element_by_xpath('//*[@id="myModal"]/div/div/div[1]/button').click()
+        print('The pop up was closed')
+    else:
+        raise Exception("Timeout while waiting the pop up. Please run the test again.")
+except:
+    pass
 
 # Printing the title of fifth position
-title_fifth_elem = browser.find_element_by_xpath('//*[@id="position5"]').text
-print("The title is '%s'" % (title_fifth_elem))
+fifth_elem = browser.find_element_by_xpath('//*[@id="position5"]')
+title_text = fifth_elem.text
+print("The title is '%s'" % (title_text))
 
-# Making a pause for ... ?
+# Making a pause for ... Implicit Waits needs to be implemented
 time.sleep(4)
 
 # Selecting 5th element in search result page
-fifth_elem = browser.find_element_by_xpath('//*[@id="position5"]').click()
-# webdriver.ActionChains(browser).move_to_element(fifth_elem).click(fifth_elem).perform()
+fifth_elem.click()
 
+# Making a pause for ... Implicit Waits needs to be implemented
 time.sleep(4)
 
-discription = browser.find_element_by_xpath('//*[@id="jobdescSec"]/p[1]').text
-print 'Job Description:"\n"', unicodedata.normalize('NFKD', discription).encode('ascii','ignore')
-
+#  Printing the description of the job
+# description = browser.find_element_by_id('jobdescSec') #  should work whit PyCharm
+# print description.text #  should work whit PyCharm
+description = browser.find_element_by_xpath('//*[@id="jobdescSec"]/p[1]').text
+print 'Job Description:"\n"', unicodedata.normalize('NFKD', description).encode('ascii', 'ignore')
