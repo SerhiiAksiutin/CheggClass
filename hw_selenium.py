8261b025891c910e97a4e7ce2a80be423e1105a8
Serhii Aksiutin <saksiutin@scml-serhii.local>
2015-07-26 17:29:55 -0700

    Clear code, Pressing Enter added

--- hw_selenium.py
+++ hw_selenium.py
@@ -7,39 +7,47 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-# import time >
-
-# Running a browser
+# Running a browser >
 driver = webdriver.Chrome()
 
-# Assigning an URL
+# Maximizing the browser >
+driver.set_window_size(1280, 800)
+driver.set_window_position(0, 0)
+
+# Assigning an URL >
 url = 'http://www.ebay.com'
 
-# Sending the URL
+# Sending the URL >
 driver.get(url)
 
-# Detecting search field and pasting the value
-search_fild = driver.find_element_by_id('gh-ac')
-search_fild.send_keys('261972139580')
+# Detecting search field and pasting the value >
+search_field = driver.find_element_by_id('gh-ac')
+search_field.send_keys('261972139580')
 
-# Clicking the 'Search' button
-search_button = driver.find_element_by_id('gh-btn')
-search_button.click()
+# Clicking the 'Search' button >
+# search_button = driver.find_element_by_id('gh-btn')
+# search_button.click()
 
-# Verifying by eBay item number
-expected_eBay_item_number = 261972139580  # This is wrong value to fail the test. The correct one is 261972139580
+# Pressing Enter >
+search_field.send_keys(Keys.RETURN)
 
+# Verifying by eBay item number >
+expected_eBay_item_number = 261972139580
 
+# Explicit waiting >
 wait = WebDriverWait(driver, 4)
 elem = wait.until(EC.presence_of_element_located((By.ID, 'descItemNumber')))
-# time.sleep(4) >
+
+# Finding and printing the item number >
 eBay_item_number = driver.find_element_by_id('descItemNumber')
 number = eBay_item_number.text
 print('=================')
 print('=  %s =' % (number))
 print('=================')
 
+# Raisign en error once the item mumber doesn't match >
 if int(number) == expected_eBay_item_number:
     print('Hooray! My first webdriver test has passed')
 else:
